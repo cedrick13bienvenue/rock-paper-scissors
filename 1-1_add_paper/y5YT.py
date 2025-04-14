5cdb4f26a158ac0a9cf25073141e325cafeb6c7f
Cedrick Bienvenue <cedrick4smith@gmail.com>
2025-04-14 20:46:01 +0200

    feat: implementing basic rock-paper-scissors game with paper hand (task 1-1)

--- 1-1_add_paper/y5YT.py
+++ 1-1_add_paper/y5YT.py
@@ -3,7 +3,6 @@ import random
 print("0: rock, 1: scissors, 2: paper") 
 player = int(input("your hand: "))  
 
-computer = random.randint(0, 2)  
-
+computer = random.randint(0, 2) 
 print("my hand: ", player)
 print("computer's hand: ", computer)
